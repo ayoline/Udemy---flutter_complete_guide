4933bd1ce341a63c599bf5551773ba31e6b3596b
Anderson P Soares <ayo@outlook.com.br>
2023-02-24 16:59:05 -0300

    calculate a total score

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/result.dart';
 
 import './quiz.dart';
 import './result.dart';
@@ -19,21 +18,39 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       "questionText": 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       "questionText": 'What\'s your favorite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
     },
     {
       "questionText": 'Who\'s your favorite instructor?',
-      'answers': ['Max', 'Ander', 'Roger', 'Renato'],
+      'answers': [
+        {'text': 'Max', 'score': 4},
+        {'text': 'Ander', 'score': 3},
+        {'text': 'Roger', 'score': 2},
+        {'text': 'Renato', 'score': 1},
+      ],
     },
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore = _totalScore + score;
+
     setState(() {
       _questionIndex += 1;
       print(_questionIndex);
